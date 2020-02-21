f7b41ca79deea1b29937c29215ad062a316ee5f4
Whisper2020 <61183451+Whisper2020@users.noreply.github.com>
2020-02-21 15:26:19 +0800

    Decode source file

---
+++ decode.cpp
@@ -0,0 +1,16 @@
+/******** decode.cpp ********/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout<<"Decoding.."<<endl;
+	return 0;
+}
+
+/* -----zhr-----*/
+
+Your code here.
+
+//END zhr.
+
