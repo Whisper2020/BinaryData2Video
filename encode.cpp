d090e15762b625bbed90dff9523ce6111f586d56
Snow02203835 <61280404+Snow02203835@users.noreply.github.com>
2020-02-21 23:09:00 +0800

    Update encode.cpp
    添加编码函数

--- encode.cpp
+++ encode.cpp
@@ -1,9 +1,13 @@
 /******** encode.cpp ********/
 #include <iostream>
+#include <cstdlib>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 //函数声明
 
 using namespace std;
+int Rows, Cols;//用于解码时读取图片的行列像素数
 //全局变量定义
 
 int main(int argc, char** argv) {
@@ -11,8 +15,22 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-/* -----zhr-----*/
-
-ZHR's code here.
-
-//END zhr.
+/* -----ldr-----*/
+void encode(char* str)
+{
+    Mat image = imread(str);
+    if (!image.data || image.channels() != 3) {
+        cout << "Can't open the picture or the picture's channels aren't 3." << endl;
+        return;
+    }
+    Rows = image.rows, Cols = image.cols;
+    FILE* filePoint = NULL;
+    const char* strs = "encoded.bin";
+    fopen_s(&filePoint, strs, "wb");
+    char* pData = (char*)image.data;
+    for (int i = 0; i < Rows * Cols; i++)
+        for (int j = 0; j < 3; j++)
+            fwrite(&pData[i * 3 + j], sizeof(char), 1, filePoint);
+    fclose(filePoint);
+}
+//END ldr.
