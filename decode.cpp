677d336db27dbe8d683b14f46917592ed88efbc3
Snow02203835 <61280404+Snow02203835@users.noreply.github.com>
2020-02-21 23:17:42 +0800

    Update decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -8,9 +8,26 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-/* -----zhr-----*/
+/* -----ldr-----*/
 
-Your code here.
+int Rows, Cols;
+void decode(char* str)
+{
+    Mat image = imread(str);
+    if (!image.data || image.channels() != 3) {
+        cout << "Can't open the picture or the picture's channels aren't 3." << endl;
+        return;
+    }
+    Rows = image.rows, Cols = image.cols;
+    FILE* filePoint = NULL;
+    const char* strs = "encoded.bin";
+    fopen_s(&filePoint, strs, "wb");
+    char* pData = (char*)image.data;
+    for (int i = 0; i < Rows * Cols; i++)
+        for (int j = 0; j < 3; j++)
+            fwrite(&pData[i * 3 + j], sizeof(char), 1, filePoint);
+    fclose(filePoint);
+}
 
-//END zhr.
+//END ldr.
 
