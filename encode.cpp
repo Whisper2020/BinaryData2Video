468ba9b76941a26d7c27d2bd8aec9a3e10c79813
Whisper2020 <61183451+Whisper2020@users.noreply.github.com>
2020-02-25 19:41:19 +0800

    test failed.

--- encode.cpp
+++ encode.cpp
@@ -5,12 +5,11 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/core.hpp>
-#include <opencv.hpp>
 
 //函数声明
-int encode(char* str1);//参数1为二进制文件的绝对位置,如果出错返回-1，正常运行返回图片的张数
-void Image_to_Video_Generate(int num, int frame_width, int frame_height, float frame_fps, float video_time);//解释在函数实现部分
-
+int encode(const char* str1);//参数1为二进制文件的绝对位置,如果出错返回-1，正常运行返回图片的张数
+void Image_to_Video_Generate(int num, float frame_fps, float video_time);//解释在函数实现部分
+int work(int argc, char** argv);
 using namespace cv;
 using namespace std;
 //全局变量定义
@@ -22,56 +21,57 @@ int main(int argc, char** argv) {
 /* -----zhr----- */
 int work(int argc, char** argv) {
 	int retCode = 0;
-	cout << "Binary file name: " << argv[1] << endl;
-	cout << "Encoding.." << endl;
-	retCode = encode(argv[1], "img");
-	cout << "Output video file name: " << argv[2] << endl;
-	cout << "Length of video: " << argv[3] << "ms" << endl; //注意argv[3]是char格式的，如果要当作数值应用atoi(argv[3])函数转换
+	//cout << "Binary file name: " << argv[1] << endl;
+	//cout << "Encoding.." << endl;
+	retCode = encode("HH");
+	Image_to_Video_Generate(1, 30.0, 5);
+	//cout << "Output video file name: " << argv[2] << endl;
+	//cout << "Length of video: " << argv[3] << "ms" << endl; //注意argv[3]是char格式的，如果要当作数值应用atoi(argv[3])函数转换
 	return retCode;
 }
 //END zhr.
 /* -----ldr----- */
-int encode(char* str1) {
-    FILE* filePoint = NULL;
-    fopen_s(&filePoint, str1, "rb");
-    if (filePoint == NULL) {
-        cout << "Failed to open file!" << endl;
-        return -1;
-    }
-    int num;//图片的张数
-    char name[20];
-    fread(&num, sizeof(int), 1, filePoint);//二进制文件先给出有多少张图片
-    Mat image(Rows, Cols, CV_8UC3, Scalar::all(0));
-    for (int i = 1; i <= num; i++) {
-        sprintf_s(name, "%s%d%s", "image", i, ".jpg");//输出图片的格式
-        char* pData = (char*)image.data;
-        for (int j = 0; j < Rows * Cols; j++)
-            for (int k = 0; k < 3; k++)
-                fread(&pData[j * 3 + k], sizeof(char), 1, filePoint);
-        imwrite(name, image);
-    }
-    fclose(filePoint);
-    return num;
+int encode(const char* str1) {
+	FILE* filePoint = NULL;
+	fopen_s(&filePoint, "in.bin", "rb");
+	if (filePoint == NULL) {
+		cout << "Failed to open file!" << endl;
+		return -1;
+	}
+	int num=1;//图片的张数
+	char name[20];
+	//fread(&num, sizeof(int), 1, filePoint);//二进制文件先给出有多少张图片
+	Mat image(Rows, Cols, CV_8UC3, Scalar::all(0));
+	for (int i = 1; i <= num; i++) {
+		sprintf_s(name, "%s%d%s", "image", i, ".jpg");//输出图片的格式
+		char* pData = (char*)image.data;
+		for (int j = 0; j < Rows * Cols; j++)
+			for (int k = 0; k < 3; k++)
+				fread(&pData[j * 3 + k], sizeof(char), 1, filePoint);
+		imwrite(name, image);
+	}
+	fclose(filePoint);
+	return num;
 }
 //End ldr
 /* -----y g----- */
-void Image_to_Video_Generate(int num,int frame_width,int frame_height,float frame_fps,float video_time)
+void Image_to_Video_Generate(int num, float frame_fps, float video_time)
 {
-//frame_height——图片高度
-//frame_width——图片宽度
-//frame_fps——图片帧率
-//num——图片帧数
-//video_time——视频播放时间，毫秒
-//图像预先存储命名为：imageXX.jpg(eg:image1,image2...)
+	//frame_height——图片高度
+	//frame_width——图片宽度
+	//frame_fps——图片帧率
+	//num——图片帧数
+	//video_time——视频播放时间，毫秒
+	//图像预先存储命名为：imageXX.jpg(eg:image1,image2...)
 	char image_name[20];
 	string s_image_name;
 	VideoWriter writer;
 	int isColor = 1;
 	string video_name = "out.avi";
 	float time = 0;//记录播放时间
-	writer = VideoWriter(video_name,writer.fourcc('X', 'V', 'I', 'D'), frame_fps, Size(frame_width, frame_height), isColor);
-	cout << "frame_width is " << frame_width << endl;
-	cout << "frame_height is " << frame_height << endl;
+	writer = VideoWriter(video_name, writer.fourcc('X', 'V', 'I', 'D'), frame_fps, Size(Rows, Cols), isColor);
+	cout << "frame_width is " << Rows << endl;
+	cout << "frame_height is " << Cols << endl;
 	cout << "frame_fps is " << frame_fps << endl;
 	int i = 0;
 	Mat img;
@@ -93,7 +93,6 @@ void Image_to_Video_Generate(int num,int frame_width,int frame_height,float fram
 		}
 		if (time >= video_time)
 			break;
-	}	
+	}
 }
 //end yg
-
