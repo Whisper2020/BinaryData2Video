cd3a7350f6ce377f2e0b746f57a28df08363ab27
Whisper2020 <61183451+Whisper2020@users.noreply.github.com>
2020-03-12 13:19:02 +0800

    Add encode tools

--- QRDecodeTools by zhr/QRCodeTools.cpp
+++ QRDecodeTools by zhr/QRCodeTools.cpp
@@ -1,8 +1,20 @@
 #include "QRCodeTools.h"
 #include <iostream>
-QRDecodeTools::QRDecodeTools(int v, float e) :Ver(v), sz(4 * (v - 1) + 21), dbg(false) {
+
+
+QRCodeTools::QRCodeTools(int v) :Ver(v), sz(4 * (v - 1) + 21), dbg(false) {}
+
+QRCodeTools::~QRCodeTools(){}
+
+void QRCodeTools::setDbg(bool flag)
+{
+	dbg = flag;
+}
+
+QRDecodeTools::QRDecodeTools(int v, float e) : QRCodeTools(v), Eps(e){
 	setThreshold(e);
 }
+
 QRDecodeTools::~QRDecodeTools()
 {
 	if (dbg)
@@ -10,7 +22,7 @@ QRDecodeTools::~QRDecodeTools()
 	img.~Mat();
 }
 
-int QRDecodeTools::checkPos(const int x, const int y) const //return 1 if err
+int QRCodeTools::checkPos(const int x, const int y) const //return 1 if err
 {
 	int chk = 0;
 	if (x < 0 || x >= sz || y < 0 || y >= sz)	chk = 1;
@@ -35,20 +47,16 @@ cv::Point2f QRDecodeTools::rate(const int p, const cv::Point2f& a, const cv::Poi
 	return cv::Point2f(f*b.x + (1-f)*a.x, f*b.y + (1-f)*a.y);
 }
 
-
 int QRDecodeTools::loadQRCode(cv::InputArray in)
 {
 	gotQR = wheels.detect(in, Rect);
+	if (!gotQR)
+		return false;
 	if (in.channels() == 3)
 		cv::cvtColor(in, img, cv::COLOR_BGR2GRAY);
 	else
 		in.getMat().copyTo(img);
-	if (gotQR) {
-		lx = Rect[1] - Rect[0];
-		ly = Rect[2] - Rect[1];
-
-	}
-	return gotQR;
+	return true;
 }
 
 int QRDecodeTools::detected()
@@ -79,14 +87,84 @@ void QRDecodeTools::setThreshold(const float x)
 	Eps = x;
 }
 
-
 void QRDecodeTools::flush()
 {
 	gotQR = 0;
 	Rect.clear();
 }
 
-void QRDecodeTools::setDbg(bool flag)
+QREncodeTools::QREncodeTools(int v):QRCodeTools(v){
+	src = cv::Mat(cv::Size(sz + 6, sz + 6), CV_8UC1, cv::Scalar(0));
+	makeSrc();
+	flush();
+}
+
+void QREncodeTools::flush()
 {
-	dbg = flag;
+	src.copyTo(img);
+}
+
+int QREncodeTools::write(const int x, const int y, const int bit)
+{
+	if (checkPos(x, y)) {
+		std::cout << "Pos error:(" << x << "," << y << ") can't be written." << std::endl;
+		return -1;
+	}
+	img.at<uchar>(cv::Point(x + 3, y + 3)) = bit ? UCHAR_MAX : 0;
+	if (dbg)
+		std::cout << "wrote " << bit << " at " << cv::Point(x, y) << std::endl;
+	return 0;
+}
+
+void QREncodeTools::mask()
+{
+}
+
+void QREncodeTools::display()const
+{
+	std::cout << "Display QRCode:" << std::endl;
+	for (int i = 0; i < sz; ++i) {
+		for (int j = 0; j < sz; ++j)
+			std::cout << (img.at<uchar>(cv::Point(i + 3, j + 3)) ? "■":"□" );
+		std::cout << std::endl;
+	}
+}
+
+cv::Size QREncodeTools::output(cv::OutputArray out, int rate)
+{
+	//img.copyTo(out);
+	cv::Size outSize(sz + 6, sz + 6);
+	outSize *= rate;
+	mask();
+	cv::resize(img, out, outSize, rate, rate, cv::INTER_NEAREST);
+	return outSize;
+}
+
+void QREncodeTools::makeSrc() //生成二维码模板
+{
+	drawLocator(6, 6); //画四个定位块
+	drawLocator(6, sz - 1);
+	drawLocator(sz - 1, 6);
+	drawLocator(sz - 1, sz - 1);
+	for (int i = 9; i < sz - 3; ++i) { //画时间线
+		src.at<uchar>(cv::Point(i, 9)) = (i & 1) ? 0 : 255;
+		src.at<uchar>(cv::Point(9, i)) = (i & 1) ? 0 : 255;
+	}
+	for (int i = 0; i < sz + 6; ++i) //清空边框
+		for (int j = 0; j < 3; ++j) {
+			src.at<uchar>(cv::Point(i, j)) = 255;
+			src.at<uchar>(cv::Point(j, i)) = 255;
+			src.at<uchar>(cv::Point(i, sz - j + 5)) = 255;
+			src.at<uchar>(cv::Point(sz - j + 5, i)) = 255;
+		}
+}
+
+void QREncodeTools::drawLocator(const int x, const int y)
+{
+	int t;
+	for (int i = x - 4; i < x + 5; ++i)
+		for (int j = y - 4; j < y + 5; ++j) {
+			t = std::max(labs(i - x), labs(j - y));
+			src.at<uchar>(cv::Point(i, j)) = (t == 2 || t == 4) ? 255 : 0;
+		}
 }
