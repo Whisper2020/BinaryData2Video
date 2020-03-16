cdd2bd86c16d99febcf2cb5b21db7865782ecef7
XMU-YG <61274975+XMU-YG@users.noreply.github.com>
2020-03-16 11:13:32 +0800

    add video to image
    直接调用函数，完成视频到图像划分

--- decode.cpp
+++ decode.cpp
@@ -44,11 +44,54 @@ void decode(char* str1, char* str2)
     fclose(filePoint);
 }
 //End ldr
-void seperate(const char* pathMp4, const char* pathPng)//seperate("test.mp4", "x%d.png");//示例
+/*视频划分为图像*/
+void Video_To_Image_Separate(string filename)
 {
-	string strFfmpegPath = "E:\\ffmpeg-4.2.2-win64-static\\bin\\ffmpeg";//ffmpeg.exe所在的位置
-	string strPathMp4 = pathMp4;//待转化的mp4
-	string strPathPng = pathPng;//生成的png
-	string strCmdContent = "/c" + strFfmpegPath + " -i " + strPathMp4 + " " + strPathPng;
-	ShellExecute(NULL, "open", "cmd", strCmdContent.c_str(), NULL, SW_HIDE);
+	VideoCapture capture(filename);
+	if (!capture.isOpened())
+	{
+		cout << "open video error";
+	}
+	/*CV_CAP_PROP_POS_MSEC – 视频的当前位置（毫秒）
+	CV_CAP_PROP_POS_FRAMES – 视频的当前位置（帧）
+	CV_CAP_PROP_FRAME_WIDTH – 视频流的宽度
+	CV_CAP_PROP_FRAME_HEIGHT – 视频流的高度
+	CV_CAP_PROP_FPS – 帧速率（帧 / 秒）*/
+	int frame_width = (int)capture.get(CAP_PROP_FRAME_WIDTH);
+	int frame_height = (int)capture.get(CAP_PROP_FRAME_HEIGHT);
+	float frame_fps = capture.get(CAP_PROP_FPS);
+	int frame_number = capture.get(CAP_PROP_FRAME_COUNT);//总帧数
+	cout << "frame_width is " << frame_width << endl;
+	cout << "frame_height is " << frame_height << endl;
+	cout << "frame_fps is " << frame_fps << endl;
+	int num = 0;//统计帧数
+	Mat img;
+	string img_name;
+	char image_name[20];
+	int kernel_size = 3;
+	/*一个简单的滤波处理，防止雪花*/
+	Mat kernel = Mat::ones(kernel_size, kernel_size, CV_32F) / (float)(kernel_size * kernel_size);
+
+	while (true)
+	{
+		Mat frame;
+		//从视频中读取一个帧
+		bool bSuccess = capture.read(frame);
+		if (!bSuccess)
+		{
+			cout << "不能从视频文件读取帧" << endl;
+			break;
+		}
+		filter2D(frame, frame, -1, kernel);
+		sprintf_s(image_name, "%s%d%s", "image", ++num, ".jpg");//保存的图片名
+		img_name = image_name;
+		/*以10fps生成的视频一般以30fps划分，重复三张，取中间一张最好*/
+		if (num%3==1)
+			imwrite(img_name, frame);//保存一帧图片
+		if (waitKey(30) == 27 || num == frame_number)
+		{
+			cout << "按下ESC键" << endl;
+			break;
+		}
+	}
 }
