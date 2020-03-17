0a979f422ca1dbee54e164529658716a7933fd82
LYR-kami <61258341+LYR-kami@users.noreply.github.com>
2020-03-18 00:42:49 +0800

    Add files via upload

---
+++ const.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef Rows
+#define Rows 61
+#endif
+#ifndef Cols
+#define Cols 61
+#endif
+#ifndef Block
+#define Block 10
+#endif
+#ifndef Frames
+#define Frames 2.0
+#endif
+#ifndef TSIZE
+#define TSIZE 61
+#endif
