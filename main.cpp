14999d457489a29e9ff0f98e095b2167b639f665
LYR-kami <61258341+LYR-kami@users.noreply.github.com>
2020-03-18 00:44:33 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "QRCodeTools.h"
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	FILE* stream1;
+	freopen_s(&stream1, "out.bin", "wb", stdout);
+	/*char str1[] = "black01.bin";
+	encode(str1);/*
+	MapToVideo();
+
+	char name[] = "T11.png";
+	MapToBinary(name);
+	cherk();*/
+	char str[] = "test01.jpg", str2[] = "1.bin";
+	/*Demo(str);
+	cherk(str2);*/
+	readFile(str2);
+	readframes();
+	return 0;
+}
+
