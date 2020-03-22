3c0143e2a5af9cbdfe2056e5feb75bcf0442e207
Snow02203835 <61280404+Snow02203835@users.noreply.github.com>
2020-03-23 00:00:22 +0800

    Create const.h

---
+++ Encode/const.h
@@ -0,0 +1,15 @@
+#ifndef Rows
+#define Rows 385
+#endif
+#ifndef Cols
+#define Cols 385
+#endif
+#ifndef TSIZE
+#define TSIZE 49
+#endif
+#ifndef FPS
+#define FPS 10.0
+#endif 
+#ifndef VERSION 
+#define VERSION 8
+#endif
